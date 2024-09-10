867001ec31fe04ff334caa784885e05f19f43fa4
Alex Tran <Lockey5891@aol.com>
2024-09-09 20:54:04 -0700

    added code to Recipe.js

---
+++ server/models/Recipe.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const recipeSchema = new Schema({
+  recipeName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  ingredients: {
+    type: String,
+    required: true,
+   
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const Recipe = model("Recipe", recipeSchema);
+
+module.exports = Recipe;
